b7ca133cb46a1fa55b893253c601479dd0583ad2
angel36r2873648327 <aserrano11@ucol.mx>
2024-05-20 23:28:42 -0600

    funciona modificar alumnos y maestros

--- Proyectos_Ordoñez1/Admin/proyecto/administrador/Modificar alumnos y maestros/modificar/Modificar_profesores.php
+++ Proyectos_Ordoñez1/Admin/proyecto/administrador/Modificar alumnos y maestros/modificar/Modificar_profesores.php
@@ -4,36 +4,36 @@
         <?php
         require_once 'contacto.php';
         $obj = new Contacto();
-        $resultado = $obj->consultar_alumnos();
+        $resultado = $obj->consultar_profesores();
         if ($resultado) {
             while ($registro = $resultado->fetch_assoc()) {
-                $selected = (isset($_POST['idmodificar']) && $_POST['idmodificar'] == $registro['matricula_alumno']) ? 'selected' : '';
+                $selected = (isset($_POST['idmodificar']) && $_POST['idmodificar'] == $registro['matricula_profesor']) ? 'selected' : '';
                 //Esta parte de aqui va a poner en el select la matricula y nombre del alumno para que pueda diferenciar el usuario
-                echo "<option value='" . $registro["matricula_alumno"] . "' $selected> " . $registro["matricula_alumno"] . " - " . $registro["nombre_completo"] . "</option>";
+                echo "<option value='" . $registro["matricula_profesor"] . "' $selected> " . $registro["matricula_profesor"] . " - " . $registro["nombre_completo"] . "</option>";
             }
         } else {
             echo "No hay registros de alumnos para modificar.";
         }
         ?>
     </select>
-    <input type="submit" name="cargar" value="Cargar Datos del alumno">
+    <input type="submit" name="cargar" value="Cargar Datos del profesor">
 </form>
 
 <?php
 if (isset($_POST["cargar"]) && isset($_POST["idmodificar"])) {
     $obj = new Contacto();
-    $resultado = $obj->consultar_alumnos($_POST["idmodificar"]);
+    $resultado = $obj->consultar_profesores($_POST["idmodificar"]);
     if ($registro = $resultado->fetch_assoc()) {
         ?>
         <form action="" method="post">
             <!--Esto de aqui va a imprimir el registro ya guardado del alumno seleccionado al presionar el boton de guardar -->
-            Matricula del alumno: <input type="text" name="matricula_alumno" value="<?php echo $registro["matricula_alumno"]; ?>" readonly><br>
+            Matricula del Profesor: <input type="text" name="matricula_profesor" value="<?php echo $registro["matricula_profesor"]; ?>" readonly><br>
             Nombre Completo: <input type="text" name="nombre_completo" value="<?php echo $registro["nombre_completo"]; ?>"><br>
-            Correo del alumno: <input type="text" name="correo" value="<?php echo $registro["correo"]; ?>"><br>
+            Correo del Profesor: <input type="text" name="correo" value="<?php echo $registro["correo"]; ?>"><br>
             Contraseña: <input type="text" name="contraseña" value="<?php echo $registro["contraseña"]; ?>"><br>
 
             <input type="hidden" name="idmodificar" value="<?php echo $_POST["idmodificar"]; ?>">
-            <input type="submit" name="modificar" value="Modificar alumno">
+            <input type="submit" name="modificar" value="Modificar profesor">
         </form>
         <?php
     } else {
@@ -43,20 +43,20 @@ if (isset($_POST["cargar"]) && isset($_POST["idmodificar"])) {
 
 if (isset($_POST["modificar"])) {
     //el if de abajo va a verificar que esten llenos los registros 
-    if (isset($_POST['matricula_alumno'], $_POST['nombre_completo'], $_POST['correo'], $_POST['contraseña'])) {
+    if (isset($_POST['matricula_profesor'], $_POST['nombre_completo'], $_POST['correo'], $_POST['contraseña'])) {
        
     
-        $matricula_alumno = $_POST['matricula_alumno'];
+        $matricula_profesor = $_POST['matricula_profesor'];
         $nombre_completo = $_POST['nombre_completo'];
         $correo = $_POST['correo'];
         $contraseña = $_POST['contraseña'];
 
 
         $obj = new Contacto();
-        $obj->modificar_alumno($matricula_alumno, $contraseña, $nombre_completo, $correo);
+        $obj->modificar_profesor($matricula_profesor, $contraseña, $nombre_completo, $correo);
         $obj->modificar_login($correo, $contraseña);
 
-        echo "Alumno y datos de login modificados";
+        echo "Profesor y datos de login modificados";
     } else {
         echo "Faltan campos por llenar.";
     }
--- Proyectos_Ordoñez1/Admin/proyecto/administrador/Modificar alumnos y maestros/modificar/contacto.php
+++ Proyectos_Ordoñez1/Admin/proyecto/administrador/Modificar alumnos y maestros/modificar/contacto.php
@@ -2,7 +2,7 @@
    include ("conexion.php");
    Class Contacto extends Conexion{
 
-
+  //metodo para obtener los registros de los alumnos
        public function consultar_alumnos($matricula_alumno = null) {
         if ($matricula_alumno !== null) {
             $this->sentencia = "SELECT * FROM alumnos WHERE matricula_alumno = '$matricula_alumno'";
@@ -13,31 +13,33 @@
         return $resultado;
     }
       
-      //metodo para consultar profesores
-      public function consultar_profesores(){
-         $this->sentencia = "SELECT * FROM profesores;";
-         $resultado = $this->obtener_sentencia(); 
-         return $resultado;
-      }
+      //metodo para obtener los registros de los profesores
+     public function consultar_profesores($matricula_profesor = null) {
+        if ($matricula_profesor !== null) {
+            $this->sentencia = "SELECT * FROM profesores WHERE matricula_profesor = '$matricula_profesor'";
+        } else {
+            $this->sentencia = "SELECT * FROM profesores";
+        }
+        return $this->obtener_sentencia();
+    }
 
       //metodo para modificar el registro del alumno
-      public function modificar_alumnos($matricula_alumno, $contraseña, $nombre_completo, $correo) {
+      public function modificar_alumno($matricula_alumno, $contraseña, $nombre_completo, $correo) {
     $this->sentencia = "UPDATE alumnos SET contraseña = '$contraseña', nombre_completo = '$nombre_completo', correo = '$correo' WHERE matricula_alumno = $matricula_alumno";
     $this->ejecutar_sentencia();
     }
       
 
       //metodo para modificar el registro del alumno
-      public function modificar_profesor($matricula_profesor,$contraseña,$nombre_completo, $correo){
-         $this->sentencia = "UPDATE contacto SET matricula_profesor='$matricula_profesor', contraseña='$contraseña', nombre_completo='$nombre_completo', correo='$correo' Where matricula_profesor= '$matricula_profesor' ";
-         $bandera = $this->ejecutar_sentencia();
-         return $bandera;
-      }
+     public function modificar_profesor($matricula_profesor, $contraseña, $nombre_completo, $correo) {
+    $this->sentencia = "UPDATE profesores SET contraseña = '$contraseña', nombre_completo = '$nombre_completo', correo = '$correo' WHERE matricula_profesor = '$matricula_profesor'";
+    $this->ejecutar_sentencia();
+}
 
-        public function modificar_login($correo, $contraseña) {
-        $this->sentencia = "UPDATE login SET contraseña = '$contraseña' WHERE correo = '$correo'";
-        $this->ejecutar_sentencia();
-    }
+public function modificar_login($correo, $contraseña) {
+    $this->sentencia = "UPDATE login SET contraseña = '$contraseña' WHERE correo = '$correo'";
+    $this->ejecutar_sentencia();
+}
   }
 
 ?>
