8de7d0f2da641139cf9acf40ef847a61574fe1b3
angel36r2873648327 <aserrano11@ucol.mx>
2024-05-23 11:33:37 -0600

    actualizado interfaz_admin

--- Proyectos_Ordoñez1/LoginTitanic/admin/login_administrador/controlador.php
+++ Proyectos_Ordoñez1/LoginTitanic/admin/login_administrador/controlador.php
@@ -1,48 +1,47 @@
 <?php
 include("conexion.php");
 
-if (isset($_POST["btn_login"])) {
-    $correo = $_POST['correo'];
-    $contraseña = $_POST['contraseña'];
-    
-    $conexion = new Conexion();
-    $conexion->sentencia = "SELECT * FROM login WHERE correo = '$correo' AND contraseña = '$contraseña'";
-    $resultado = $conexion->obtener_sentencia();
-    
-    if ($resultado->num_rows > 0) {
-        // Verificar el rol del usuario
-        $conexion->sentencia = "SELECT * FROM administradores WHERE correo = '$correo'";
-        $resultado_admin = $conexion->obtener_sentencia();
-        
-        if ($resultado_admin->num_rows > 0) {
-            // Redirigir a la interfaz de administrador
-            header("Location: admin/login_administrador/interfaz_administrador.php");
+//ALUMNO
+if (isset($_POST["agregar_alumno"])) {
+    // Redirigir a agregar alumno
+            header("Location: administrador/Agregar alumnos y maestros/Agregar_alumno.php");
             exit();
-        } else {
-            $conexion->sentencia = "SELECT * FROM profesores WHERE correo = '$correo'";
-            $resultado_prof = $conexion->obtener_sentencia();
-            
-            if ($resultado_prof->num_rows > 0) {
-                // Redirigir a la interfaz de profesor
-                header("Location: Pagina_Maestro_prototipo1/Profesor_1.php");
-                exit();
-            } else {
-                $conexion->sentencia = "SELECT * FROM alumnos WHERE correo = '$correo'";
-                $resultado_alumno = $conexion->obtener_sentencia();
-                
-                if ($resultado_alumno->num_rows > 0) {
-                    // Redirigir a la interfaz de alumno
-                    header("Location: inicioalumno.php");
-                    exit();
-                } else {
-                    // Usuario no encontrado en ninguna tabla
-                    echo "Usuario no encontrado.";
-                }
-            }
-        }
-    } else {
-        // Credenciales incorrectas
-        echo "Correo o contraseña incorrectos.";
-    }
 }
+if (isset($_POST["eliminar_alumno"])) {
+    // Redirigir a eliminar alumno
+            header("Location: administrador/Eliminar alumnos y maestros/Eliminar_alumno.php");
+            exit();
+}
+if (isset($_POST["modificar_alumno"])) {
+    // Redirigir a modificar alumno
+            header("Location: administrador/Modificar alumnos y maestros/Modificar_alumnos.php");
+            exit();
+}
+if (isset($_POST["matricular_alumno"])) {
+    // Redirigir a matricular_alumno alumno
+            header("Location: administrador/Agregar alumnos y maestros/Agregar_alumno.php");
+            exit();
+}
+
+//MAESTROS
+
+if (isset($_POST["agregar_profesor"])) {
+    // Redirigir a agregar alumno
+            header("Location: administrador/Agregar alumnos y maestros/Agregar_profesor.php");
+            exit();
+}
+
+if (isset($_POST["eliminar_profesor"])) {
+    // Redirigir a agregar alumno
+            header("Location: administrador/Eliminar alumnos y maestros/Eliminar_profesor.php");
+            exit();
+}
+
+if (isset($_POST["modificar_profesor"])) {
+    // Redirigir a agregar alumno
+            header("Location: administrador/Modificar alumnos y maestros/Modificar_pro-+fesores.php");
+            exit();
+}
+    
+
 ?>
