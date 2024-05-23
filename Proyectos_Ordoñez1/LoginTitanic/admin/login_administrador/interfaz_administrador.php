dfd82de02ddb8f541df68b43c1517cda27eeab21
angel36r2873648327 <aserrano11@ucol.mx>
2024-05-23 10:17:50 -0600

    cambios en carpetas

---
+++ Proyectos_Ordoñez1/LoginTitanic/admin/login_administrador/interfaz_administrador.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Interfaz de administrador</title>
+    <link rel="stylesheet" type="text/css" href="ProfesorCSS_1.css">
+</head>
