ef84c2ce1c298b036033f07f9aabef6af9f1f222
JordaAMG <jmiramontes4@ucol.mx>
2024-06-06 09:59:01 -0600

    Prueba

--- Proyectos_Ordoñez1/LoginTitanic/Alumno_Padre/Verificarjustificantes_alumnopadre.php
+++ Proyectos_Ordoñez1/LoginTitanic/Alumno_Padre/Verificarjustificantes_alumnopadre.php
@@ -1,84 +1,84 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>horarios</title>
-	<link rel="stylesheet" type="text/css" href="principalhorario.css">
-</head>
-<body>
-	<div id="caja">
-		<h1 id="t">Bienvenido al Apartado de Verificar Justificantes</h1>
-	</div>
-	<div id="caja2">
-    <?php
- require_once("Contacto.php");
-      $obj= new Contacto();
-      $resultado= $obj->obtenerjustificantes_padrealumno();
-      
-      while($registro=$resultado->fetch_assoc()){
-
-      	echo "<table>";
-            echo"<tr>";
-                echo "<th>Nombre Completo</th>";
-                echo "<th>Estado del Justificante</th>";
-            echo "</tr>";
-            echo"<tr>";
-
-echo "<td>".$registro["nombre_completo"]."</td>";
-
-
-                echo "<td>".$registro["nombre_completo"]."</td>";
-                echo "<td>". $registro["justificante"]. "Aceptado" : "Rechazado"; "/td>"
-            echo "</tr>"
-        echo "</table>"
-      }
-    ?>
-         
-	</div>
-<footer id="Pie_Pagina"> <p>Correo: Motellano@mtn.com             Celular: 312- 345 - 4321</p> </footer>
-</body>
-</html>
-
-
-
-
-
-
-<!DOCTYPE html>
-<html>
-<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>horarios</title>
+    <title>Profesores de un Alumno</title>
     <link rel="stylesheet" type="text/css" href="principalhorario.css">
 </head>
 <body>
     <div id="caja">
-        <h1 id="t">Bienvenido al Apartado de Verificar Justificantes</h1>
+        <h1 id="t">Bienvenido al Apartado de Listar Calificaciones</h1>
     </div>
     <div id="caja2">
-        <?php
+    <?php
         require_once("Contacto.php");
-        $obj= new Contacto();
-        $resultado= $obj->obtenerjustificantes_padrealumno();
-        
-        echo "<table>";
-        echo "<tr>";
-        echo "<th>Nombre Completo</th>";
-        echo "<th>Estado del Justificante</th>";
-        echo "</tr>";
-        
-        while($registro=$resultado->fetch_assoc()){
-            echo "<tr>";
-            echo "<td>".$registro["nombre_completo"]."</td>";
-            echo "<td>". $registro["justificante"] ? "Aceptado" : "Rechazado" . "</td>";
-            echo "</tr>";
+        $obj = new Contacto();
+        $Matricula_Alumno = $_COOKIE['matricula'];
+
+        $resultado = $obj->consultar_Materias($Matricula_Alumno); // Llama al método consultar_Materias de la clase Contacto
+        if ($resultado->num_rows > 0) {
+            while ($fila = $resultado->fetch_assoc()) {
+                echo '<a href="?opc1=' . $fila["id_materia"] . '">' . $fila["nombre"] . '</a><br>';
+            }
+        } else {
+            echo "El alumno no esta matriculado en ninguna materia.";
         }
-        
-        echo "</table>";
         ?>
+
+        <?php
+        if (isset($_REQUEST["opc1"])) {
+            ?>
+            <form action="" method="post" id="falta">
+            <p>Fechas en las que a faltado</p>
+            <select id="Lista_falta" name="Faltas_justificante" value="Fecha_Falta"> 
+            <?php
+            $Materia_Elegida = $_REQUEST["opc1"];
+            $resultado = $obj->Listar_Faltas($Matricula_Alumno);
+            if ($resultado->num_rows > 0) {
+            // Mostrar las faltas
+            echo "Fechas en las que a faltado";
+            while ($fila = $resultado->fetch_assoc()) {
+                echo "<option  value='" . $fila["fecha"] . "'>" . $fila["fecha"] . 
+                '<a href="?opc2=' . $fila["fecha"] . '">  </a>' .  
+                "</option>";
+            }
+            ?>
+            <input type="submit" id="btn_faltas" value="Pedir justificante" name="Pedir justificante">
+
+            <?php
+            }
+            if(isset($_REQUEST['Pedir justificante'])){
+                $Fecha_Justificante = $_REQUEST['Faltas_justificante'];
+                $Fase_Justificante = 'Proceso';
+
+                require_once("Contacto.php");
+                $obj = new Contacto();
+                $obj->Solicitud_JustificanteF1($Fecha_Justificante, $Fase_Justificante);
+
+                $resultado = $obj->verificar_Justificante($Fecha_Justificante);
+            if ($resultado->num_rows > 0) {
+                echo "<table>";
+                echo "<tr>";
+                echo "<th>Estado en que se encuentra el Justificante</th>";
+                echo "</tr>";
+                while ($fila = $resultado->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $fila["justificante"] . "</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }
+            }
+        }
+                            
+        ?>
+        </select>
+    </form>
+
+    
+
     </div>
-    <footer id="Pie_Pagina"> <p>Correo: Motellano@mtn.com Celular: 312-345-4321</p> </footer>
+<footer id="Pie_Pagina"> <p>Correo: Motellano@mtn.com             Celular: 312- 345 - 4321</p> </footer>
 </body>
 </html>
