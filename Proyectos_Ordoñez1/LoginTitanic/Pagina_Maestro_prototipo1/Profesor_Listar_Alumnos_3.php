f4d8573c60404357a30974ab275a6455e824e01f
angel36r2873648327 <aserrano11@ucol.mx>
2024-06-06 11:46:15 -0600

    css de Listar alumnos

--- Proyectos_Ordoñez1/LoginTitanic/Pagina_Maestro_prototipo1/Profesor_Listar_Alumnos_3.php
+++ Proyectos_Ordoñez1/LoginTitanic/Pagina_Maestro_prototipo1/Profesor_Listar_Alumnos_3.php
@@ -1,106 +1,90 @@
 <!DOCTYPE html>
+<html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<tie></title>
-	<link rel="stylesheet" type="text/css" href="Listar_Alumnos_Profesor.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Listar Alumnos</title>
+    <link rel="stylesheet" type="text/css" href="css/Profesor_Listar_Alumnos.css">
 </head>
 <body>
     <div id="total">
-    <div id="Prinsipal">
+        <h1 id="Titulo_Principal">Listar Alumnos</h1>
+        <div id="Prinsipal">
 
-    	<div id="Color_arriba0"></div>
-	    <div id="Color_arriba1"></div>
+            <div id="Color_arriba0"></div>
+            <div id="Color_arriba1"></div>
 
-    	<header id="cabeza">
-    		<article id="Info_usuario">
-                <input id="Imagen0" type="image" src="icon_usuario.PNG" alt="Enviar" width="60px" height="60px">
-                <a href=""></a>
-                <div id="Info_usuario1">
-            	    <p>Nombre del Profesor:</p>
-                    <?php
-                        if (isset($_COOKIE['username'])) {
-                        $username = $_COOKIE['username'];
-                        echo "$username";
-                        } else {
-                            echo "No se ha encontrado el nombre.";
-                        }
-                    ?>
-                    <p>Numero de cuenta:</p>
-                    <?php
-                        if (isset($_COOKIE['matricula'])) {
-                        $matricula = $_COOKIE['matricula'];
-                            echo "$matricula";
-                        } else {
-                            echo "No se ha encontrado la matrícula.";
-                        }
-                    ?>
-               </div>
-            </article>
-
-            <article id="Info_ListAlumn">
-            	<div>
-            		Listar Alumnos:
-            	</div>
-            </article>
-
-            <article id="cerrar_secion0">
-            	<a href="?opc=cerrar_secion"> <li>Cerrar Secion</li> </a>
-                <input id="Imagen0" type="image" src="Icon_salir.PNG" alt="Enviar" width="30px" height="30px">
-            </article>
-    	</header>
-
-    	<section>
-    		<article id="Acciones">
-    			<div id="Tabla0">
-    				<header id="Titulo_Tabla">
-    					<p>Alumnos:</p>
-    				</header>
-
-                    <section>
+            <header id="cabeza">
+                <article id="Info_usuario">
+                    <div id="Info_usuario1">
+                        <p>Nombre del Profesor:</p>
                         <?php
-                        require_once("Contacto.php"); // Incluye el archivo Contacto.php que contiene la definición de la clase Contacto
+                            if (isset($_COOKIE['username'])) {
+                            $username = $_COOKIE['username'];
+                            echo "$username";
+                            } else {
+                                echo "No se ha encontrado el nombre.";
+                            }
+                        ?>
+                        <p>Numero de cuenta:</p>
+                        <?php
+                            if (isset($_COOKIE['matricula'])) {
+                            $matricula = $_COOKIE['matricula'];
+                                echo "$matricula";
+                            } else {
+                                echo "No se ha encontrado la matrícula.";
+                            }
+                        ?>
+                   </div>
+                </article>
 
-                        $obj = new contacto(); // Crea una nueva instancia de la clase Contacto
+                <article id="Info_ListAlumn">
+                    <div>
+                    </div>
+                </article>
+            </header>
 
-                        $matricula_profesor = $_COOKIE['matricula']; // Define la matrícula del profesor a consultar 
+            <section>
+                <article id="Acciones">
+                    <div id="Tabla0">
+                        <header id="Titulo_Tabla">
+                            <p>Alumnos:</p>
+                        </header>
 
-                        $resultado = $obj->consultar_Materias($matricula_profesor); // Llama al método consultar_Materias de la clase Contacto, pasando la matrícula del profesor como parámetro y almacena el resultado en la variable $resultado
+                        <section>
+                            <?php
+                            require_once("Contacto.php");
 
-                        // Verifica si la consulta devolvió algún resultado
-                        if ($resultado->num_rows > 0) {
-                        // Si hay resultados, recorre cada fila del resultado
-                        while ($fila = $resultado->fetch_assoc()) {
-                        // Muestra el nombre de la materia como un enlace HTML
-                            echo '<a href="?opc1=' . $fila["id_materia"] . '">' . $fila["nombre"] . '</a>';
-                            echo '<br>'; // Salto de línea para separar cada materia
-                        }
-                        } else {
-                            // Si no hay resultados, muestra un mensaje indicando que el profesor no imparte ninguna materia
-                            echo "El profesor no imparte ninguna materia.";
-                        }
+                            $obj = new contacto();
+                            $matricula_profesor = $_COOKIE['matricula'];
+                            $resultado = $obj->consultar_Materias($matricula_profesor);
 
-
-                        if (isset($_REQUEST["opc1"])) {
-
-                            $Materia_Elegida = $_REQUEST["opc1"];
-                            $resultado = $obj->consultar_Grupos($matricula_profesor, $Materia_Elegida);
                             if ($resultado->num_rows > 0) {
-                            // Mostrar las materias
                                 while ($fila = $resultado->fetch_assoc()) {
-                                    echo '<a href="?opc2=' . $fila["id_grupo"] . '">' . $fila["nombre_grupo"] . ' ' . $fila["semestre"] .'</a>';
+                                    echo '<a href="?opc1=' . $fila["id_materia"] . '">' . $fila["nombre"] . '</a>';
                                     echo '<br>';
                                 }
                             } else {
-                                echo "La materia no tiene grupos";
+                                echo "El profesor no imparte ninguna materia.";
                             }
-                        } 
-                        if (isset($_REQUEST["opc2"])) {
 
+                            if (isset($_REQUEST["opc1"])) {
+                                $Materia_Elegida = $_REQUEST["opc1"];
+                                $resultado = $obj->consultar_Grupos($matricula_profesor, $Materia_Elegida);
+                                if ($resultado->num_rows > 0) {
+                                    while ($fila = $resultado->fetch_assoc()) {
+                                        echo '<a href="?opc2=' . $fila["id_grupo"] . '">' . $fila["nombre_grupo"] . ' ' . $fila["semestre"] .'</a>';
+                                        echo '<br>';
+                                    }
+                                } else {
+                                    echo "La materia no tiene grupos";
+                                }
+                            }
+
+                            if (isset($_REQUEST["opc2"])) {
                                 $Grupo_Elegido = $_REQUEST["opc2"];
                                 $resultado = $obj->Listar_Alumnos($Grupo_Elegido);
                                 if ($resultado->num_rows > 0) {
-                                    // Mostrar las materias
                                     echo "<table>";
                                     echo "<tr>";
                                     echo "<th>nombre_completo</th>";
@@ -114,18 +98,20 @@
                                     echo "El grupo no tiene alumnos";
                                 }
                             }
-                    ?>
-                    
-                    </section>
+                            ?>
+                        </section>
+                    </div>
+                </article>
+            </section>    
 
-    			</div>
-    		</article>
-    	</section>    
+            <footer id="Pie_Pagina"> 
+                <p>Correo: Motellano@mtn.com             Celular: 312- 345 - 4321</p> 
+            </footer>
+        </div>
 
-    	<footer id="Pie_Pagina"> <p>Correo: Motellano@mtn.com             Celular: 312- 345 - 4321</p> </footer>
-
-
-    </div>
-    </div>	
+        <article id="volver">
+            <a href="../Pagina_Maestro_prototipo1/Profesor_1.php"> <li>Volver</li> </a>
+        </article>
+    </div>  
 </body>
 </html>
